b4fa2cdba675fb3373defec01970d9b86c16209d
zizzany <gloria.videloup@hotmail.com>
2014-01-18 12:26:18 +0100

    Create ft_iterative_power.c

---
+++ ft_iterative_power.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int         ft_iterative_power(int nb, int power)
+{
+    int     result;
+
+    result = 1;
+    if ((nb == 0 && power !=0) || power < 0)
+        return (0);
+    if ((power == 0) || (( nb < 0) && (power == 0)))
+        return (1);
+    else
+    {
+        while (power > 0)
+        {
+            result =  nb * result;
+            power--;
+        }
+        return (result);
+    }
+}
